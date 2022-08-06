86ba07c84d92e510d323e2eeb48381649d7b4a90
joker69216 <hamaraad883@gmail.com>
2022-08-06 18:02:56 +0300

    feature/ added menu toggles

--- Starter/assets/js/main.js
+++ Starter/assets/js/main.js
@@ -1,7 +1,36 @@
 // Grab elements
+const selector = (selected) =>{
+    const element = document.querySelector(selected);
+    if (element != null) 
+    return element;
+    else
+    throw new Error(`Element not found: ${selected}, check main.js`);
+}
+
 
 //Nav styles on scroll
 
+const scrollHeader = () =>{
+    const headerElement = selector('#header');
+    if(this.scrollY >= 15){
+        headerElement.classList.add('activated');
+    }
+    else{
+        headerElement.classList.remove('activated');
+    }
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+const menuToggleIcon = selector('#menu-toggle-icon');
+
+const toggleMenu = ()=>{
+    const mobileMenu = selector('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
 // Open menu & search pop-up
 
 // Open/Close search form popup
